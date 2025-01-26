b2a48165ca031cd1f5d5178af630cf970aa1263d
Thomas Roeblitz <thomas.roblitz@uib.no>
2025-01-26 19:17:04 +0000

    first components for creating, listing and deleting views

--- src/components/VirtualDataInfrastructure.vue
+++ src/components/VirtualDataInfrastructure.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    <h1>Virtual Data Infrastructure</h1>
+  <div id="virtualdatainfrastructure">
+    <VDICSSGrid
+      :selectedViewId="selectedViewId"
+      :selectedViewName="selectedViewName"
+      @update:selectedView="handleUpdateSelectedView"
+    />
   </div>
 </template>
 
 <script>
+import VDICSSGrid from './VDICSSGrid.vue';
+
 export default {
-  name: 'VirtualDataInfrastructure',
+  components: {
+    VDICSSGrid
+  },
+  data() {
+    return {
+      selectedViewId: null,
+      selectedViewName: null,
+    };
+  },
+  methods: {
+    handleUpdateSelectedView(newViewId, newViewName) {
+      console.log(`received 'update:selectedView(${newViewId}, ${newViewName})'`);
+      this.selectedViewId = newViewId;
+      this.selectedViewName = newViewName;
+    }
+  }
 };
 </script>
+
+<style>
+/* Global styles (if any) */
+#virtualdatainfrastructure {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
